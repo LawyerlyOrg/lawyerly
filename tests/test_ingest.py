90794a2f9d7ab40d4e0609f6c80f2b8b16f92c14
Monir Imamverdi <monir.imamverdi@gmail.com>
2023-08-15 15:17:17 -0600

    added db reference, removed constants reference

--- tests/test_ingest.py
+++ tests/test_ingest.py
@@ -3,7 +3,6 @@ import pinecone
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import constants
 from ingest import *
 from db import *
 from langchain.embeddings.openai import OpenAIEmbeddings
