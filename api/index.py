604160da95fd17cc209b878cc0002d9126322054
Monir Imamverdi <monir.imamverdi@gmail.com>
2023-08-15 18:21:20 -0600

    confirmed fetching and adding collections through API

--- api/index.py
+++ api/index.py
@@ -4,12 +4,13 @@ import os
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from flask import Flask, request, jsonify
+from flask_restx import Api, Resource, reqparse
 from gpt_search import chat_with_gpt
 import openai
 import pinecone
 from langchain.embeddings.openai import OpenAIEmbeddings
 from pymongo import MongoClient
-from db import get_user_collections
+from db import get_user_collections, insert_new_collection
 
 app = Flask(__name__)
 
@@ -51,8 +52,26 @@ def get_collections(user_email):
     # return a JSON response with the data and status code 200
     return jsonify(data), 200
 
-@app.route('/users/<user_id>/collections', methods=['POST'])
-def create_collection(user_id):
+@app.route('/users/<string:user_email>/collections', methods=['POST'])
+def create_collection(user_email):
+    # http://10.1.1.1:5000/users/user_email/collections?name=Haji's Case
+
+    # create a parser object
+    parser = reqparse.RequestParser()
+    # add name and description arguments
+    parser.add_argument("name", required=True, help="Collection Name cannot be blank!")
+    parser.add_argument("description", required=True, help="Collection Description cannot be blank!")
+    # parse the arguments from the request
+    args = parser.parse_args()
+
+    collection_name = args["name"]
+    collection_description = args["description"]
+
+    print(f'user email: {user_email}')
+    print(f'collection name: {collection_name}')
+    print(f'collection description: {collection_description}')
+
+    insert_new_collection(user_email, collection_name, collection_description)
 
     # return a success message and the collection id and status code 201
     return {'message': 'Collection created successfully'}, 201
