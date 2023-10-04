9bf7fd039b44b543371486380d66c882f76a02d2
Rene Reid <rdreid01@students.cs.ubc.ca>
2023-10-04 15:51:24 -0600

    tried using tutorial code...

---
+++ fe/src/components/FactsheetForm.vue
@@ -0,0 +1,32 @@
+<template>
+    <form @submit.prevent="onSubmit">
+      <input type="file" @change="handleFileUpload" />
+      <button type="submit">Upload</button>
+    </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: 'FileUpload',
+  setup() {
+    const file = ref(null);
+
+    const handleFileUpload = (event) => {
+      file.value = event.target.files[0];
+    };
+
+    const onSubmit = () => {
+      console.log("selected file", file.value);
+      // Upload to server Call API
+    };
+
+    return {
+      handleFileUpload,
+      onSubmit,
+      file
+    };
+  }
+};
+</script>
