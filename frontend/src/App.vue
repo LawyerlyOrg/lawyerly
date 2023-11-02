b311e3f9f4db39c31e197d9805d1fc5f71a53b87
Rene Reid <rdreid01@students.cs.ubc.ca>
2023-11-02 17:41:38 -0600

    navbar updated

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,103 +1,37 @@
-<!-- <template>
-  <SelectFactSheet v-bind:child-prop="selectedItem" v-on:custom-event="updateSelectedItem"/>
-  <SelectCases v-bind:child-prop="cases" v-on:custom-event="updateCases"/>
-  <RelevanciesReport :selected="selectedItem" :sharedCases="cases"/>
-</template> -->
-
-<!-- <template>
-
-<nav class="navbar">
-  <div class="navbar">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="@/assets/abstract_geometric_design.png" width="112" height="112">
-    </a>
-  </div>
-</nav>
-  <div class="container">
-    
-    
-
-    <div class="columns is-one-half is-gapless">
-      <div class="column custom-column">
-        <SelectFactSheet
-          v-bind:child-prop="selectedItem"
-          v-on:custom-event="updateSelectedItem"
-        />
-      </div>
-      <div class="column custom-column">
-        <SelectCases
-          v-bind:child-prop="cases"
-          v-on:custom-event="updateCases"
-        />
-      </div>
-    </div>
-    <RelevanciesReport :selected="selectedItem" :sharedCases="cases" />
-  </div>
-</template>
-
-<script>
-import SelectFactSheet from "./components/SelectFactSheet.vue";
-import SelectCases from "./components/SelectCases.vue";
-import RelevanciesReport from "./components/RelevanciesReport.vue";
-
-export default {
-  name: "App",
-  components: { SelectFactSheet, SelectCases, RelevanciesReport},
-  data() {
-    return {
-      selectedItem: "",
-      cases: [],
-    };
-  },
-  methods: {
-    updateSelectedItem(newValue) {
-      this.selectedItem = newValue;
-    },
-    updateCases(newCases) {
-      this.cases = newCases;
-    },
-  },
-};
-</script>
-
-<style scoped>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-body {
-  margin: 0;
-  background: white;
-}
-
-.container {
-  max-width: 800px; /* or whatever width you prefer */
-}
-
-@media screen and (min-width: 960px) {
-  .container {
-    width: 800px; /* or your preferred width */
-  }
-}
-</style> -->
-
 // Source: https://www.vuescript.com/form-wizard-stepper/
 <template>
-  <nav class="navbar" aria-label="main navigation">
-  <div class="navbar">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="@/assets/abstract_geometric_design.png" alt:="DEMO">
-    </a>
-  </div>
-</nav>
+  <!-- <nav class="navbar" aria-label="main navigation">
+    <div class="navbar">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="@/assets/abstract_geometric_design.png" alt:="DEMO">
+      </a>
+    </div>
+  </nav> -->
+
+  <nav
+    class="navbar is-fixed-top"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://github.com/LawyerlyOrg/lawyerly">
+        <img src="@/assets/lawyerly_logo.png" width="112" height="28" />
+      </a>
+    </div>
+  </nav>
+
   <div id="app">
     <div class="container">
-      <h1 class="title">Lawyerly Demo</h1>
+
+      <h6 class="subtitle is-6" style="text-align: left; margin-bottom: 0; margin-left: 10px">
+        <strong>Lawyerly</strong> is a GPT-powered research tool designed to
+        save lawyers time by distilling mountains of legal documents into information relevant to their case!
+      </h6>
+
+      <h6 class="subtitle is-6" style="text-align: left; margin-left: 10px; margin-top: 5px; color: grey">
+        <em>Created using: Flask (Python),  Vue.js, MongoDB, OpenAI API, Langchain, Pinecone</em>
+      </h6>
+
       <Wizard
         squared-tabs
         card-background
@@ -114,7 +48,6 @@ body {
           {
             title: 'Generate Relevancies',
           },
-          
         ]"
         :beforeChange="onTabBeforeChange"
         @change="onChangeCurrentTab"
@@ -122,19 +55,20 @@ body {
       >
         <h5 v-if="currentTabIndex === 0">
           <div>
-            <SelectFactSheet v-bind:child-prop="selectedItem" v-on:custom-event="updateSelectedItem"/>
+            <SelectFactSheet
+              v-bind:child-prop="selectedItem"
+              v-on:custom-event="updateSelectedItem"
+            />
           </div>
-        
         </h5>
         <h5 v-if="currentTabIndex === 1">
-          
-          <SelectCases v-bind:child-prop="cases" v-on:custom-event="updateCases"/>
-        
+          <SelectCases
+            v-bind:child-prop="cases"
+            v-on:custom-event="updateCases"
+          />
         </h5>
         <h5 v-if="currentTabIndex === 2">
-          
-          <RelevanciesReport :selected="selectedItem" :sharedCases="cases"/>
-        
+          <RelevanciesReport :selected="selectedItem" :sharedCases="cases" />
         </h5>
       </Wizard>
     </div>
@@ -147,21 +81,19 @@ body {
 </template>
 
 <script>
-import 'form-wizard-vue3/dist/form-wizard-vue3.css';
-import Wizard from 'form-wizard-vue3';
+import "form-wizard-vue3/dist/form-wizard-vue3.css";
+import Wizard from "form-wizard-vue3";
 import SelectFactSheet from "./components/SelectFactSheet.vue";
 import SelectCases from "./components/SelectCases.vue";
 import RelevanciesReport from "./components/RelevanciesReport.vue";
 
-
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Wizard,
     SelectFactSheet,
     SelectCases,
     RelevanciesReport,
-
   },
   data() {
     return {
@@ -183,20 +115,20 @@ export default {
     },
     onTabBeforeChange() {
       if (this.currentTabIndex === 0) {
-        console.log('First Tab');
+        console.log("First Tab");
       }
-      console.log('All Tabs');
+      console.log("All Tabs");
     },
     wizardCompleted() {
-      console.log('Wizard Completed');
+      console.log("Wizard Completed");
     },
   },
   computed: {
     nextButtonOptions() {
       return this.currentTabIndex === 2
         ? {
-            text: 'test',
-            icon: 'check',
+            text: "test",
+            icon: "check",
             hideIcon: true, // default false but selected for sample
             hideText: false, // default false but selected for sample
             disabled: true,
@@ -214,12 +146,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 5rem;
 }
 
 .container {
   max-width: 800px; /* or whatever width you prefer */
 }
-
-
 </style>
--- frontend/src/components/RelevanciesReport.vue
+++ frontend/src/components/RelevanciesReport.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <button class="button is-primary" v-if="selected && !loading" @click="getRelevancy">
+      <button class="button is-primary" v-if="selected && !loading && !isHidden" @click="getRelevancy(); isHidden=true">
         Generate
       </button>
       <div v-if="loading">
@@ -12,28 +12,29 @@
     <!-- <div v-for="(item, key) in sharedCases" :key="key">
       <td>{{ item.name }}</td>
     </div> -->
-    
+
     <div class="has-text-left">
-      <table class="table is-bordered is-striped is-hoverable is-narrow">
-        <thead v-if="relevancies === true">
+      <table class="table is-hoverable is-fullwidth" v-if="displayContent">
+        <thead >
           <tr>
             <th>Case</th>
             <th>Relevancy</th>
           </tr>
         </thead>
-        <tbody v-if="relevancies">
+        <tbody>
           <tr v-for="(item, key) in relevancies" :key="key">
             <td>{{ sharedCases.find((element) => element._id.$oid === key).name }}</td>
             <td class="break-newlines">{{ item }}</td>
           </tr>
         </tbody>
       </table>
+  
 
-      <table class="table is-bordered is-striped is-hoverable is-narrow">
+      <!-- <table class="table is-hoverable is-fullwidth">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Gender</th>
+            <th>Case</th>
+            <th>Relevancy</th>
           </tr>
         </thead>
         <tbody>
@@ -42,7 +43,8 @@
             <td class="break-newlines">{{ person.gender }}</td>
           </tr>
         </tbody>
-      </table>
+      </table> -->
+      
     </div>
   </div>
 </template>
@@ -68,27 +70,32 @@ export default {
       fact_sheet_id: "",
       relevancies: [],
       loading: false,
+      isHidden: false,
+      displayContent: false,
       people: [
-        { name: "Jane Smith", gender: "- Both the summary and the factsheet mention the topic of law and legal reforms.\n- Both mention the concept of self-defence.\n- Both mention the purpose of simplifying the legislative text.\n- Both mention the removal of preconditions in the old law.\n- Both mention a three-part framework in the new law.\n- Both mention a specific case involving a person named Allison and her partner Daniel." },
-        { name: "Alice Johnson", gender: "- Both the summary and the factsheet mention a case involving a trial judge's error of law.\n- Both mention the concept of party liability.\n- Both mention the need for a new trial.\n- Both mention the Criminal Code and its treatment of principal offenders and parties to an offense.\n- Both mention the importance of considering all theories of liability based on the evidence." },
-        { name: "Bob Brown", gender: "- Both the Summary and the Factsheet mention Canada and its legal system.\n- Both mention the topic of self-defence.\n- Both mention the purpose of the reforms to simplify the legislative text and facilitate the application of self-defence principles.\n- Both mention the removal of preconditions and the creation of a single, three-part defence.\n- Both mention a person named Daniel and his sexual relationship with a person named Allison." },
-        { name: "Charlie Davis", gender: "Non-Binary" },
+         { name: "JaneJaneJaneJane Smith", gender: "- Both the summary and the factsheet mention the topic of law and legal reforms.\n- Both mention the concept of self-defence.\n- Both mention the purpose of simplifying the legislative text.\n- Both mention the removal of preconditions in the old law.\n- Both mention a three-part framework in the new law.\n- Both mention a specific case involving a person named Allison and her partner Daniel." },
+         { name: "Alice Johnson", gender: "- Both the summary and the factsheet mention a case involving a trial judge's error of law.\n- Both mention the concept of party liability.\n- Both mention the need for a new trial.\n- Both mention the Criminal Code and its treatment of principal offenders and parties to an offense.\n- Both mention the importance of considering all theories of liability based on the evidence." },
+         { name: "Bob Brown", gender: "- Both the Summary and the Factsheet mention Canada and its legal system.\n- Both mention the topic of self-defence.\n- Both mention the purpose of the reforms to simplify the legislative text and facilitate the application of self-defence principles.\n- Both mention the removal of preconditions and the creation of a single, three-part defence.\n- Both mention a person named Daniel and his sexual relationship with a person named Allison." },
+         { name: "Charlie Davis", gender: "Non-Binary" },
       ],
     };
   },
   methods: {
     getRelevancy() {
-      console.log(this.selected);
+      console.log("this is the fact sheet: " + this.selected);
       this.loading = true;
       axios
         .get(
-          "https://lawyerlyservice.uw.r.appspot.com/relevancies?collection_id=65149bcca1b526a820ee1892&fact_sheet_id=" +
+          "https://lawyerlyservice.uw.r.appspot.com/relevancies?collection_id=6536eeb20c27a16e16d0ad92&fact_sheet_id=" +
             this.selected,
           { params: { selected: this.selected } }
         )
         .then((response) => {
           // handle response data
           this.relevancies = response.data;
+          if (response.data != null) {
+            this.displayContent = true;
+          }
           console.log(this.relevancies);
           this.loading = false;
         })
